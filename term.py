324f924b465e433cec8cdf0b56738ac0f6456df1
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-10-12 21:48:23 +0200

    basic example with two windows in a grid and the first output from tmux printed correctly (with support for ANSI escape codes)

--- term.py
+++ term.py
@@ -5,21 +5,52 @@ gi.require_version('Vte', '2.91')
 from gi.repository import Gtk, Vte
 from gi.repository import GLib
 import os
+import time
+import subprocess
+from threading import Thread
 
-terminal     = Vte.Terminal()
-terminal.spawn_sync(
-    Vte.PtyFlags.DEFAULT,
-    os.environ['HOME'],
-    ["/bin/sh"],
-    [],
-    GLib.SpawnFlags.DO_NOT_REAP_CHILD,
-    None,
-    None,
+def new_term():
+    terminal = Vte.Terminal()
+    terminal.spawn_sync(
+        Vte.PtyFlags.DEFAULT,
+        os.environ['HOME'],
+        [],
+        [],
+        GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+        None,
+        None,
     )
+    terminal.set_vexpand(True)
+    terminal.set_hexpand(True)
+    return terminal
 
 win = Gtk.Window()
 win.connect('delete-event', Gtk.main_quit)
-win.add(terminal)
+grid = Gtk.Grid()
+t1 = new_term()
+grid.attach(t1,0,0,1,1)
+grid.attach(new_term(),1,0,2,1)
+win.add(grid)
 win.show_all()
+
+def handle():
+    tmux = subprocess.Popen(["tmux","-C"],stdout=subprocess.PIPE,stdin=None)
+    for l in tmux.stdout:
+        if l.startswith(b"%begin"):
+            pass
+        elif l.startswith(b"%end"):
+            pass
+        elif l.startswith(b"%output"):
+            cmd = b" ".join(l.split()[2:])
+            out = cmd.decode('unicode_escape').encode()
+            t1.feed(out)
+            print("out: ",l)
+        else:
+            print("unsupported command: ",l)
+
+    
+
+t = Thread(target=handle)
+t.start()
 
 Gtk.main()
